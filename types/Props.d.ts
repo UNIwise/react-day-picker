a4c3093c2a69e7d965fbbd355ba344e47081728a
dimitar.nizamov <dimitur2204@gmail.com>
2022-02-15 09:23:06 +0100

    Removed git merge symbold from the file

--- types/Props.d.ts
+++ types/Props.d.ts
@@ -16,11 +16,7 @@ export interface CaptionElementProps {
 }
 
 export interface OverlayComponentProps {
-<<<<<<< HEAD
   classNames: InputClassNames;
-=======
-  classNames: ClassNames;
->>>>>>> d97112186903e85aebd167d47ec15449dcaff1ef
   month: Date;
   selectedDay: Date;
   input: any;
@@ -29,13 +25,7 @@ export interface OverlayComponentProps {
   hideDayPicker: () => void;
   getDayPicker: () => React.Ref<DayPicker>;
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> d9711218 (Add hideDayPicker and getDayPicker methods to OverlayComponent)
-=======
->>>>>>> d97112186903e85aebd167d47ec15449dcaff1ef
 export interface NavbarElementProps {
   className: string;
   classNames: ClassNames;
